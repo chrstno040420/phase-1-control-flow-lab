6e57c0b77614b5129acaa57d64143fdf55a5baf8
chrstno040420 <104917222+chrstno040420@users.noreply.github.com>
2022-06-06 22:02:17 -0500

    commit tree 46b00ec39bcb2c58ee7cb57a6b213e39538ddcda

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride <= 400){
+    return ("This one is on me!");
+  } else if(ride > 400, ride < 2000 ){
+    return ("That will be twenty bucks.");
+  } else if (ride <= 2500) {
+    return ("I will gladly take your thirty bucks.");
+  }else if (ride > 2500){
+    return ("No can do.");
+  }
+  return ride
+}
+function ternaryCheckCity(city){
+  // Write your code here
+
+  return ((city === "NYC") ? 'Ok, sounds good.' : 'No go.');
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(greeting){
+  // Write your code here
+
+  switch(greeting){
+  case"generous":
+      return "Thank you so much.";
+  case"not as generous":
+    return "Thank you.";
+  default:
+    return "Bye.";
+}
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+scuberGreetingForFeet(199);
+scuberGreetingForFeet(1500);
+scuberGreetingForFeet(2001);
+scuberGreetingForFeet(2501);
+
+ternaryCheckCity("NYC");
+ternaryCheckCity("Pittspurgh");
+
+switchOnCharmFromTip('generous');
+switchOnCharmFromTip("not as generous");
+scuberGreetingForFeet();
+
+
